8c6880f7bb663b4f3c5e21720bcb9002679a2106
Rafael <rafaelskz92@gmail.com>
2023-02-16 14:53:53 -0300

    Subindo o header e algumas adições de props no button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <Button text="Teste" color="bg-brand1" textColor="text-whiteFixed" />
     </>
   );
