8bd2964e465c222d328428831c4ad0a2b6fcb889
Enrique <enrique.barbosasilva@gmail.com>
2023-02-17 15:48:31 -0300

    refactor: validations registerAnnoucement

--- src/validations/forms.validations.ts
+++ src/validations/forms.validations.ts
@@ -1,12 +1,12 @@
 import * as yup from "yup";
 
 export const registerAnnoucementSchema = yup.object().shape({
-  typeAnnoucement: yup.string(),
-  title: yup.string(),
-  year: yup.number(),
-  mileage: yup.string(),
-  price: yup.string(),
-  description: yup.string(),
-  typeVehicle: yup.string(),
-  image: yup.string(),
+  typeAnnoucement: yup.string().required("O tipo de anuncio é obrigatório"),
+  title: yup.string().required("Título é obrigatório"),
+  year: yup.number().required("Ano é obrigatório"),
+  mileage: yup.string().required("Quilometragem é obrigatório"),
+  price: yup.string().required("Preço é obrigatório"),
+  description: yup.string().required("Descrição obrigatória"),
+  typeVehicle: yup.string().required("O tipo de veiculo é obrigatório"),
+  image: yup.string().required("Imagem é obrigatória"),
 });
