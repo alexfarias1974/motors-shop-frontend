8fe6a80eb9e3ab774329501068b1d3cf994723f5
Enrique <enrique.barbosasilva@gmail.com>
2023-02-17 11:37:06 -0300

    build: new libs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import Form from "./components/Form";
+import ModalBase from "./components/ModalBase";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <>
-      <Button text="Teste" color="bg-brand1" textColor="text-whiteFixed" />
+      <button
+        onClick={() => {
+          setIsOpenModal(true);
+        }}
+      >
+        opa
+      </button>
+
+      {isOpenModal ? (
+        <ModalBase setIs={setIsOpenModal}>
+          <Form accountSubmit={() => {}} />
+        </ModalBase>
+      ) : null}
     </>
   );
 }
