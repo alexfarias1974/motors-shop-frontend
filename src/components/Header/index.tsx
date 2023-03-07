2c6664cb8c7e2cc553184fd4bd839c92b53c3c31
=Welton Santos <weltonsantosfr@gmail.com>
2023-03-07 16:50:02 -0300

    fix: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Logo from "../../assets/Motors shop.svg";
 import Button from "../Button";
 import NavBar from "../NavBar";
@@ -29,7 +29,7 @@ const Header = () => {
           console.log(err);
         });
     }
-  }, []);
+  }, [isActiveNavBar]);
 
   return (
     <>
@@ -52,15 +52,21 @@ const Header = () => {
         </div>
         <div className="max-md:hidden flex justify-around items-center h-20">
           <div className="flex gap-11 mr-11">
-            <span className="font-semibold text-grey2 cursor-pointer">
+            <a className="font-semibold text-grey2 cursor-pointer" href="#cars">
               Carros
-            </span>
-            <span className="font-semibold text-grey2 cursor-pointer">
+            </a>
+            <a
+              className="font-semibold text-grey2 cursor-pointer"
+              href="#motorcycles"
+            >
               Motos
-            </span>
-            <span className="font-semibold text-grey2 cursor-pointer">
+            </a>
+            <a
+              className="font-semibold text-grey2 cursor-pointer"
+              href="#auction"
+            >
               Leilão
-            </span>
+            </a>
           </div>
           <div className="flex items-center gap-11 h-20 border-grey6 border-l-2">
             {isLogin ? (
@@ -104,20 +110,30 @@ const Header = () => {
       {isActiveMenu && (
         <div className="md:hidden shadow-[0px_40px_40px_rgba(0,0,0,0.09)] absolute w-[100%]">
           <div className="h-[14.75rem] bg-whiteFixed flex flex-col gap-11 py-[2rem] px-4 border-b-[1px] border-grey4">
-            <span className="font-semibold text-grey2">Carros</span>
-            <span className="font-semibold text-grey2">Motos</span>
-            <span className="font-semibold text-grey2">Leilão</span>
+            <a className="font-semibold text-grey2" href="#cars">
+              Carros
+            </a>
+            <a className="font-semibold text-grey2" href="#motorcycles">
+              Motos
+            </a>
+            <a className="font-semibold text-grey2" href="#auction">
+              Leilão
+            </a>
           </div>
           <div className="flex flex-col gap-11 bg-whiteFixed h-[11.5rem] py-[2rem] px-4">
-            <span className="font-semibold text-grey2">Fazer Login</span>
+            <span
+              className="font-semibold text-grey2"
+              onClick={() => navigate("/login")}
+            >
+              Fazer Login
+            </span>
             <div className="flex">
-              <Button
-                text="Cadastrar"
-                color="bg-grey10"
-                textColor="grey0"
-                border="border-2"
-                borderColor="border-grey4"
-              />
+              <button
+                className="bg-grey10 text-grey0 border-2 border-grey4 hover:text-whiteFixed hover:bg-grey1 h-12 max-md-2[20.813rem] md:w-36 w-full rounded font-semibold text-base"
+                onClick={() => navigate("/register")}
+              >
+                Cadastrar
+              </button>
             </div>
           </div>
         </div>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -42,8 +42,8 @@ const NavBar = (accountType: any) => {
 
   return (
     <>
-      {accountType === "buyer" ? (
-        <div className="flex flex-col justify-between p-5 w-[12.5rem] h-[12.5rem]  bg-grey9 rounded shadow-[0_4px_40px_-10px_rgba(0,0,0,0.25)]">
+      {accountType.accountType === "buyer" ? (
+        <div className="flex flex-col justify-between p-5 w-[12.5rem] h-[10rem]  bg-grey9 rounded shadow-[0_4px_40px_-10px_rgba(0,0,0,0.25)]">
           <span
             className="text-grey2 cursor-pointer"
             onClick={() => setIsModalEditOpen(true)}
@@ -51,7 +51,6 @@ const NavBar = (accountType: any) => {
             Editar Perfil
           </span>
           <span className="text-grey2 cursor-pointer">Editar Endereço</span>
-          <span className="text-grey2 cursor-pointer">Minhas Compras</span>
           <span
             className="text-grey2 cursor-pointer"
             onClick={() => {
